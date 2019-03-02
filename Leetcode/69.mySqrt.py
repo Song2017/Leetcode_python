2b85b287b7772fb85c9f2afbbcd2dea2a132636e
unknown <bensong2017@hotmail.com>
2019-03-02 19:58:57 +0800

    leet 69

---
+++ Leetcode/69.mySqrt.py
@@ -0,0 +1,7 @@
+class Solution:
+    def mySqrt(self, x: int) -> int:
+        if x <= 1: return x
+        pre, rtn = 0, 1.1
+        while abs(rtn - pre) > 1:
+            pre, rtn = rtn, (rtn + x/rtn)/2
+        return int(rtn)
