0b2e35772e35529f4f8eee2ddf246ba35f4d03ca
unknown <bensong2017@hotmail.com>
2019-10-13 09:05:43 +0800

    update readme.md

--- _cache.py
+++ _cache.py
@@ -1,2 +1,12 @@
-a = {a:123}
-a.popi
+## 随机产生一个4位数随机码
+import random
+def randomcode(code_len = 4):
+    all_code = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    last_code = len(all_code) - 1
+    code = " "
+    for _ in range(code_len):
+        index = random.randint(0,last_code)
+        print(index, code[1])
+        code += code[index]
+    return(code)
+randomcode()
